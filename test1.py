4e3412f2729d6557af968ebc3c04ccbbe46b76e1
knut-sdpark <sdp1422@naver.com>
2019-05-22 15:12:35 +0900

    modified - '라벨링 색 구분 코드 수정' - 'test1.py'

--- test1.py
+++ test1.py
@@ -18,17 +18,36 @@ def showImage():
     # Thresholding
     ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     cv2.imshow('binary image', threshold)
+
     # Opening
     kernel = np.ones((3,3), np.uint8)
     threshold = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=1)
+
     # Labeling
     ret, markers = cv2.connectedComponents(threshold)
-    cnt = np.amax(markers)
-    print('number of labels = ', cnt)
-    # display markers
-    markers = markers * (254/cnt)
-    markers = markers.astype(np.uint8)
-    cv2.imshow('labeled image', markers)
+    # cnt = np.amax(markers)
+    # print('number of labels = ', cnt)
+    #
+    # # display markers
+    # markers = markers * (254/cnt)
+    # markers = markers.astype(np.uint8)
+
+    ################################
+    ###### 라벨링 색깔입히기 #######
+    labels = markers
+    label_hue = np.uint8(179 * labels / np.max(labels))
+    blank_ch = 255 * np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+    # cvt to BGR for display
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+    # set bg label to black
+    labeled_img[label_hue == 0] = 0
+    ################################
+
+    # cv2.imshow('labeled image', markers)
+    cv2.imshow('labeled image', labeled_img)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
