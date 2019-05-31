b3314dd1d260294d159b5da21ed190e695289a1b
knut-sdpark <sdp1422@naver.com>
2019-05-31 17:30:02 +0900

    updated 'python document'

--- test2.py
+++ test2.py
@@ -5,24 +5,27 @@ from tkinter.filedialog import askopenfilename
 
 '''
 
-> 논문 제목
+# Region Labeling Project
+
+## 논문 제목
 - Fast Region Labeling with Boundary Tracking
 
-> 논문 저자
+## 논문 저자
 - Hironobu Takahashi / Fumiaki Tomita
 
-> 작성자
+## 작성자
 - 한국교통대학교 소프트웨어학과 1444009 박상돈
-- github : https://github.com/sdp1422/region_processing_project
+- Github : [https://github.com/sdp1422/region_processing_project](https://github.com/sdp1422/region_processing_project)
 
-> 최종 구현 일자
+## 최종 구현 일자
 - 2019/05/30
 
-> 구현 목적
-- 영상정보처리 2번째 레포트 과제
+## 구현 목적
+- 4학년 1학기 영상정보처리 2번째 레포트 과제
 - 논문을 읽고 이해한 후, Region Labeling 알고리즘을 구현하고, 간단한 영상을 이치화 시킨 후, 레이블링 된 결과를 캡쳐해서 보고서를 작성한 후 제출하시오.
 
-> 참고 사항
+## 참고 사항
+- 적응 이치화 적용
 - 논문 해석 오역에 따른 잘못된 코드 구현이 있을 수 있습니다.
 
 '''
